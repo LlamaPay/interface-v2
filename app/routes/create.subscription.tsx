eb5f8ae615ed9d3c6902c122e7dccd2070af96d7
mintdart <96025197+mintdart@users.noreply.github.com>
2023-12-16 07:36:00 -0600

    update create page

--- app/routes/create.subscription.tsx
+++ app/routes/create.subscription.tsx
@@ -19,7 +19,10 @@ export const meta: MetaFunction = () => {
 export default function Create() {
 	const [receiver, setReceiver] = useState("");
 	const [amount, setAmount] = useState("");
-	const url = `https://llamapay.io/subscribe?address=${receiver}&amount=${amount}`;
+	const [bgColor, setBgColor] = useState("#23BF91");
+	const url = `https://llamapay.io/subscribe?address=${receiver}&amount=${amount}&brandColor=${encodeURIComponent(
+		bgColor
+	)}`;
 	return (
 		<main className="relative mx-auto flex w-full max-w-[450px] flex-col gap-5 px-4 py-9 md:-left-[102px]">
 			<Link to="/" className="flex items-center gap-1 text-[#70757d] dark:text-[#9CA3AF]">
@@ -28,7 +31,7 @@ export default function Create() {
 			</Link>
 
 			<form className="flex flex-col gap-4">
-				<h1 className="mb-4 text-center text-xl font-medium">Create Subscription</h1>
+				<h1 className="mb-4 text-center text-xl font-medium">Create A Subscription Page</h1>
 				<label className="relative flex flex-col gap-1">
 					<span>Receiver Address</span>
 					<input
@@ -75,22 +78,39 @@ export default function Create() {
 					</span>
 				</label>
 
+				<label className="flex cursor-pointer flex-col gap-1">
+					<span>Brand Color</span>
+					<span className="flex w-full items-center gap-2 rounded-lg border border-black/[0.15] bg-[#ffffff] p-3 dark:border-white/5 dark:bg-[#141414]">
+						<input
+							type="color"
+							name="backgroundColor"
+							className="h-[1.875rem] w-[1.875rem]"
+							value={bgColor}
+							onChange={(e) => {
+								setBgColor(e.target.value);
+							}}
+						/>
+
+						<p className="flex-1">{bgColor}</p>
+					</span>
+				</label>
+
 				<p className="flex flex-col gap-1">
 					<span>Subscription URL</span>
-					<span className="w-full break-all rounded-lg border border-black/[0.15]  bg-[#ffffff] p-3 dark:border-white/5 dark:bg-[#141414]">
+					<span className="w-full break-all rounded-lg border border-black/[0.15]  bg-black/10 p-3 dark:border-white/5 dark:bg-white/10">
 						{url}
 					</span>
 				</p>
 
-				<button
-					type="button"
-					onClick={() => {
-						navigator.clipboard.writeText(url ?? "");
-					}}
-					className="rounded-lg bg-[#13785a] p-3 text-white disabled:opacity-60 dark:bg-[#23BF91] dark:text-black"
+				<a
+					target="_blank"
+					rel="noreferrer noopener"
+					href={url}
+					className="flex flex-nowrap items-center justify-center gap-1 rounded-lg bg-[#13785a] p-3 text-white disabled:opacity-60 dark:bg-[#23BF91] dark:text-black"
 				>
-					Copy
-				</button>
+					<span>Try it out</span>
+					<Icon name="arrow-up-right" className="h-4 w-4" />
+				</a>
 			</form>
 		</main>
 	);
