2e367b1232f96bb688304ac362efd7209a3b445d
mintdart <96025197+mintdart@users.noreply.github.com>
2023-12-18 02:21:20 -0600

    let users know they earn interest on deposits

--- app/routes/subscribe.tsx
+++ app/routes/subscribe.tsx
@@ -338,7 +338,9 @@ export default function Index() {
 							} p-3 text-sm `}
 						>
 							<span>Real Cost:</span>
-							{realCostFuture && realCostFuture < 0 ? (
+							{amountToDeposit.length <= 0 ? (
+								""
+							) : realCostFuture && realCostFuture < 0 ? (
 								<>
 									<span>Free</span>
 									<Ariakit.TooltipProvider showTimeout={0}>
@@ -367,6 +369,27 @@ export default function Index() {
 								<>
 									<img src={DAI_OPTIMISM.img} width={14} height={14} alt="" />
 									<span>{`${formatNum(realCostFuture, 2) ?? 0} DAI per month`}</span>
+									<Ariakit.TooltipProvider showTimeout={0}>
+										<Ariakit.TooltipAnchor
+											render={
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 20 20"
+													fill="currentColor"
+													className="h-5 w-5"
+												>
+													<path
+														fillRule="evenodd"
+														d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM8.94 6.94a.75.75 0 11-1.061-1.061 3 3 0 112.871 5.026v.345a.75.75 0 01-1.5 0v-.5c0-.72.57-1.172 1.081-1.287A1.5 1.5 0 108.94 6.94zM10 15a1 1 0 100-2 1 1 0 000 2z"
+														clipRule="evenodd"
+													/>
+												</svg>
+											}
+										/>
+										<Ariakit.Tooltip className="max-w-xs cursor-default border border-solid border-black bg-white p-1 text-sm text-black">
+											{`You will earn 5% APR on your deposits`}
+										</Ariakit.Tooltip>
+									</Ariakit.TooltipProvider>
 								</>
 							)}
 						</p>
