95e9b0f7d921b583a3381cc2869ed97dddd70085
mintdart <96025197+mintdart@users.noreply.github.com>
2024-02-28 01:13:48 -0600

    fix

--- app/root.tsx
+++ app/root.tsx
@@ -138,7 +138,7 @@ function App() {
 					sizes="16x16"
 					href="/favicon-16x16.png"
 				/>
-				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="manifest" href="/manifest.json" />
 				<style>{rootStyle}</style>
 				<Meta />
 				<Links />
